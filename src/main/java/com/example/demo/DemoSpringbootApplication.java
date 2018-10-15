ac83f17589dccc7c8261c0bfde09e2238b72000e
Vaidyanath Rajpoot <M1040042@mindtree.com>
2018-10-16 05:22:08 +0530

    update

--- src/main/java/com/example/demo/DemoSpringbootApplication.java
+++ src/main/java/com/example/demo/DemoSpringbootApplication.java
@@ -2,11 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoSpringbootApplication {
+public class DemoSpringbootApplication extends SpringBootServletInitializer  {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoSpringbootApplication.class, args);
-	}
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(DemoSpringbootApplication.class);
+	    }
+	 
+	    public static void main(String[] args) throws Exception {
+	        SpringApplication.run(DemoSpringbootApplication.class, args);
+}
 }
